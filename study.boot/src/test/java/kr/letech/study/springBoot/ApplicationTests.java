04a255fb21ee27fbbd51e4fb2af13afc097b2ca5
ChoiDazzi <choidazzi@gmail.com>
2024-01-05 18:46:02 +0900

    d

--- study.boot/src/test/java/kr/letech/study/springBoot/ApplicationTests.java
+++ study.boot/src/test/java/kr/letech/study/springBoot/ApplicationTests.java
@@ -2,8 +2,12 @@ package kr.letech.study.springBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import kr.letech.study.boot.Application;
 
 @SpringBootTest
+@ContextConfiguration(classes = Application.class)
 class ApplicationTests {
 
 	@Test
